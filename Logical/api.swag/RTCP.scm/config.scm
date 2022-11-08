41afc15c286b54b8ffa4e2957f9e5f4a862d5eff
sysadmin <sysadmin@userver.ibm.demo>
2022-11-08 17:49:00 +0000

    openapi with stub

--- Logical/api.swag/RTCP.scm/config.scm
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?><editableResource createdTimestamp="1666856181488" createdUser="sysadmin" id="3dff0d7a:18418539674:-7ecc:2525743:-1834518822" type="service_component_resource" updatedTimestamp="1666856181488" updatedUser="sysadmin" version="1.1">
-   <syncdetails compare="RTCP" lastSynced="1666856181488" sourceid="3dff0d7a:18418539674:-7ecc"/>
-   <loggingConfiguration debug="false" writeStubEventsToDB="false"/>
-</editableResource>
